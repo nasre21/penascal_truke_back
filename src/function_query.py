e8287d388b20ce140aa369333344b1ec0986c242
gorka1999 <gorkaibarrondo23@gmail.com>
2023-07-19 10:43:57 +0200

    incluido la parte de files que se había perdido

--- src/function_query.py
+++ src/function_query.py
@@ -1,16 +1,28 @@
 import src.database as db
 from flask import request, jsonify, session
 import jwt
+import json
 
+import cloudinary
+import cloudinary.uploader
+
+from src.cloudinary_credentials import cloud_name, api_key, api_secret
 
 database_path = ""
+
+
 
 # function to connect to the database
 
 def init_db(database):
     global database_path
     database_path = database
-    
+
+cloudinary.config(
+    cloud_name=cloud_name,
+    api_key=api_key,
+    api_secret=api_secret
+)
 
 # function to get all the products from the database, returns them in an array
 
@@ -29,13 +41,9 @@ def get_products():
             else:
                 decoded_product[product_col_Names[i]] = value
         product_array.append(decoded_product)
-
     cursor.close()
     return product_array
-
-
 # function to get all the products of a specific category from the database, returns them in array
-
 def get_category(category):
     con = db.connectdb()
     cursor = con.cursor()
@@ -48,9 +56,6 @@ def get_category(category):
         categorys_array.append(dict(zip(categorys_col_Names, categorys)))
     cursor.close()
     return categorys_array
-
-
-
 # function to get all the users that are in the database for the administrator
 def get_users_data():
     con = db.connectdb()
@@ -61,10 +66,8 @@ def get_users_data():
     user_col_Names = [column[0] for column in cursor.description]
     for user in myusers:
         user_array.append(dict(zip(user_col_Names, user)))
-
     cursor.close()
     return user_array
-
 #funtion to get one product
 def get_one_product(id_product):
     con = db.connectdb()
@@ -86,15 +89,27 @@ def create_product(data):
     cursor = con.cursor()
     data = request.get_json()
     files= data["files"]
+    print("esto es files", data["files"])
     name = data["name"]
     description = data["description"]
     price = data["price"]
     userid = data["userid"]
+    iduser = data["iduser"]
     category = data["category"]
     cursor.execute('INSERT INTO product (files, name, description, price, category, userid) VALUES (%s, %s, %s, %s, %s, %s)', (files, name, description, price, category, userid,))
+
+    # Subir las imágenes a Cloudinary
+    uploaded_images = []
+    for file in files:
+        upload_result = cloudinary.uploader.upload(file)
+        uploaded_images.append(upload_result["secure_url"])
+
+    uploaded_images_json = json.dumps(uploaded_images)
+
+    cursor.execute('INSERT INTO product (files, name, description, price, category, iduser) VALUES (%s, %s, %s, %s, %s, %s)', (uploaded_images_json, name, description, price, category, iduser,))
     con.commit()
     con.close()
-    
+
     print('product added successfully')
     
     return "Product created successfully"
@@ -107,32 +122,23 @@ def change_product(id_product, data):
     if "files" in data:
         files= data["files"]
         cursor.execute('UPDATE product SET files= %s WHERE idproduct = %s', (files, id_product))
-
     if "name" in data:
         name = data["name"]
         cursor.execute('UPDATE product SET name = %s WHERE idproduct = %s', (name, id_product))
-
     if "description" in data:
         description = data["description"]
         cursor.execute('UPDATE product SET description = %s WHERE idproduct = %s', (description, id_product))
-
     if "price" in data:
         price = data["price"]
         cursor.execute('UPDATE product SET price = %s WHERE idproduct = %s', (price, id_product))
-
     if "category" in data:
         category = data["category"]
         cursor.execute('UPDATE product SET category = %s WHERE idproduct = %s', (category, id_product)) 
-
               
     con.commit()
     con.close()
-
     return 'Dates modified'
-
-
 # function to delete a product
-
 def delete_data_product(idproduct):
     con = db.connectdb()
     cursor = con.cursor()
@@ -140,13 +146,10 @@ def delete_data_product(idproduct):
     con.commit()
     con.close()
     return 'Product deleted'
-
-
 # join function user with product
 def join_product_user(product_id):
     con = db.connectdb()
     cursor = con.cursor()
-
     query = """
         SELECT *
         FROM product
@@ -155,10 +158,8 @@ def join_product_user(product_id):
     """
     cursor.execute(query, (product_id,))
     result = cursor.fetchone()
-
     cursor.close()
     con.close()
-
     if result:
         product = {
             'idproduct': result[0],
